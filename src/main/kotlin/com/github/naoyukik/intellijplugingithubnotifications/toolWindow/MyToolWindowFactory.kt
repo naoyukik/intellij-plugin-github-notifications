9cc063983b7d414cda09509805bc71b856521a28
Kodama Naoyuki <mmaga@dstribe.net>
2024-12-07 17:07:42 +0900

    refactor: Improve code readability with better formatting by detekt

--- src/main/kotlin/com/github/naoyukik/intellijplugingithubnotifications/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/naoyukik/intellijplugingithubnotifications/toolWindow/MyToolWindowFactory.kt
@@ -1,5 +1,7 @@
 package com.github.naoyukik.intellijplugingithubnotifications.toolWindow
 
+import com.github.naoyukik.intellijplugingithubnotifications.MyBundle
+import com.github.naoyukik.intellijplugingithubnotifications.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
@@ -8,15 +10,15 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import com.github.naoyukik.intellijplugingithubnotifications.MyBundle
-import com.github.naoyukik.intellijplugingithubnotifications.services.MyProjectService
 import javax.swing.JButton
-
 
 class MyToolWindowFactory : ToolWindowFactory {
 
     init {
-        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
+        thisLogger().warn(
+            "Don't forget to remove all non-needed sample code files with " +
+                "their corresponding registration entries in `plugin.xml`."
+        )
     }
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
@@ -35,11 +37,13 @@ class MyToolWindowFactory : ToolWindowFactory {
             val label = JBLabel(MyBundle.message("randomLabel", "?"))
 
             add(label)
-            add(JButton(MyBundle.message("shuffle")).apply {
-                addActionListener {
-                    label.text = MyBundle.message("randomLabel", service.getRandomNumber())
+            add(
+                JButton(MyBundle.message("shuffle")).apply {
+                    addActionListener {
+                        label.text = MyBundle.message("randomLabel", service.getRandomNumber())
+                    }
                 }
-            })
+            )
         }
     }
 }
--- src/test/kotlin/com/github/naoyukik/intellijplugingithubnotifications/MyPluginTest.kt
+++ src/test/kotlin/com/github/naoyukik/intellijplugingithubnotifications/MyPluginTest.kt
@@ -1,12 +1,12 @@
 package com.github.naoyukik.intellijplugingithubnotifications
 
+import com.github.naoyukik.intellijplugingithubnotifications.services.MyProjectService
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.openapi.components.service
 import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
-import com.github.naoyukik.intellijplugingithubnotifications.services.MyProjectService
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
