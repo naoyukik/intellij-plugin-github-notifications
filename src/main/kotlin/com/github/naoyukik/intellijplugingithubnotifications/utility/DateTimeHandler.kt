8cd44af9614c3739fb88b48777dbd8d614bc52fc
Kodama Naoyuki <mmaga@dstribe.net>
2025-02-02 21:40:44 +0900

    feat: add function to subtract minutes and format to ISO-8601

--- src/main/kotlin/com/github/naoyukik/intellijplugingithubnotifications/utility/DateTimeHandler.kt
+++ src/main/kotlin/com/github/naoyukik/intellijplugingithubnotifications/utility/DateTimeHandler.kt
@@ -10,4 +10,11 @@ object DateTimeHandler {
         val instant = Instant.parse(utcDate)
         val localDateTime = ZonedDateTime.ofInstant(instant, zoneId)
         return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(localDateTime)
-    } }
+    }
+
+    fun minusNMinutesIso8601(now: ZonedDateTime, minutes: Long): String {
+        val updatedTime = now.minusMinutes(minutes)
+        val formatter = DateTimeFormatter.ISO_INSTANT
+        return formatter.format(updatedTime)
+    }
+}
