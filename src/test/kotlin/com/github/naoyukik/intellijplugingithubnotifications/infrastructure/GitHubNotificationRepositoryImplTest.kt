cfc5c9971c650846da30027502b531c509e02266
Kodama Naoyuki <mmaga@dstribe.net>
2025-02-13 22:23:59 +0900

    test: add unit tests for NotificationDetail and repository

--- src/test/kotlin/com/github/naoyukik/intellijplugingithubnotifications/infrastructure/GitHubNotificationRepositoryImplTest.kt
+++ src/test/kotlin/com/github/naoyukik/intellijplugingithubnotifications/infrastructure/GitHubNotificationRepositoryImplTest.kt
@@ -1,5 +1,6 @@
 package com.github.naoyukik.intellijplugingithubnotifications.infrastructure
 
+import com.github.naoyukik.intellijplugingithubnotifications.domain.model.NotificationDetailResponse
 import com.github.naoyukik.intellijplugingithubnotifications.utility.CommandExecutor
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
@@ -64,6 +65,158 @@ class GitHubNotificationRepositoryImplTest : StringSpec({
 
         shouldThrow<Exception> {
             repository.fetchNotifications(ghCliPath)
+        }
+    }
+
+    "fetchNotificationsReleaseDetail should return a valid NotificationDetailResponse when valid data is returned" {
+        val ghCliPath = "testPath"
+        val repositoryName = "test/repo"
+        val detailApiPath = "issues/1"
+
+        val validJson = """
+            * Request at 2025-02-13 21:58:31.8861502 +0900 JST m=+0.118526501
+            * Request to https://api.github.com/notifications
+            > GET /notifications HTTP/1.1
+
+            < HTTP/2.0 200 OK
+
+            {
+                "id": "1",
+                "state": "open",
+                "merged": false,
+                "draft": false,
+                "html_url": "https://github.com/test/repo/issues/1",
+                "requested_reviewers": []
+            }
+        """.trimIndent()
+
+        mockkObject(CommandExecutor)
+        every {
+            CommandExecutor.execute(
+                listOf(
+                    ghCliPath,
+                    "api",
+                    "/repos/$repositoryName/$detailApiPath",
+                    "--verbose",
+                ),
+            )
+        } returns validJson
+
+        val repository = GitHubNotificationRepositoryImpl()
+        val result = repository.fetchNotificationsReleaseDetail(
+            ghCliPath = ghCliPath,
+            repositoryName = repositoryName,
+            detailApiPath = detailApiPath,
+        )
+
+        result shouldBe NotificationDetailResponse.NotificationDetail(
+            state = "open",
+            merged = false,
+            draft = false,
+            htmlUrl = "https://github.com/test/repo/issues/1",
+            requestedReviewers = emptyList(),
+        )
+    }
+
+    "fetchNotificationsReleaseDetail should return a valid NotificationDetailError when Accessed unauthorized data" {
+        val ghCliPath = "testPath"
+        val repositoryName = "test/repo"
+        val detailApiPath = "issues/1"
+
+        val validJson = """
+            * Request at 2025-02-13 21:58:31.8861502 +0900 JST m=+0.118526501
+            * Request to https://api.github.com/notifications
+            > GET /notifications HTTP/1.1
+
+            < HTTP/2.0 200 OK
+
+            {
+              "message": "Not Found",
+              "documentation_url": "https://docs.github.com/rest/releases/releases#get-a-release",
+              "status": "404"
+            }
+        """.trimIndent()
+
+        mockkObject(CommandExecutor)
+        every {
+            CommandExecutor.execute(
+                listOf(
+                    ghCliPath,
+                    "api",
+                    "/repos/$repositoryName/$detailApiPath",
+                    "--verbose",
+                ),
+            )
+        } returns validJson
+
+        val repository = GitHubNotificationRepositoryImpl()
+        val result = repository.fetchNotificationsReleaseDetail(
+            ghCliPath = ghCliPath,
+            repositoryName = repositoryName,
+            detailApiPath = detailApiPath,
+        )
+
+        result shouldBe NotificationDetailResponse.NotificationDetailError(
+            message = "Not Found",
+            documentationUrl = "https://docs.github.com/rest/releases/releases#get-a-release",
+            status = "404",
+        )
+    }
+
+    "fetchNotificationsReleaseDetail should throw an exception when invalid JSON is returned" {
+        val ghCliPath = "testPath"
+        val repositoryName = "test/repo"
+        val detailApiPath = "issues/1"
+        val invalidJson = "invalid json"
+
+        mockkObject(CommandExecutor)
+        every {
+            CommandExecutor.execute(
+                listOf(
+                    ghCliPath,
+                    "api",
+                    "/repos/$repositoryName/$detailApiPath",
+                    "--verbose",
+                ),
+            )
+        } returns invalidJson
+
+        val repository = GitHubNotificationRepositoryImpl()
+
+        shouldThrow<Exception> {
+            repository.fetchNotificationsReleaseDetail(
+                ghCliPath = ghCliPath,
+                repositoryName = repositoryName,
+                detailApiPath = detailApiPath,
+            )
+        }
+    }
+
+    "fetchNotificationsReleaseDetail should throw an exception when no data is returned" {
+        val ghCliPath = "testPath"
+        val repositoryName = "test/repo"
+        val detailApiPath = "issues/1"
+
+        mockkObject(CommandExecutor)
+        every {
+            CommandExecutor.execute(
+                listOf(
+                    ghCliPath,
+                    "api",
+                    "/repos/$repositoryName/$detailApiPath",
+                    "--verbose",
+                ),
+            )
+        } returns null
+
+        val repository = GitHubNotificationRepositoryImpl()
+
+        shouldThrow<IllegalArgumentException> {
+            repository.fetchNotificationsReleaseDetail(
+                ghCliPath = ghCliPath,
+                repositoryName = repositoryName,
+                detailApiPath = detailApiPath,
+            )
         }
     }
 })
