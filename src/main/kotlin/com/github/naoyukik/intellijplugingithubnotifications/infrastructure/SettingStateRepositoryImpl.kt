24ae44e5cfafc24f68fb11367accc642e839632e
Kodama Naoyuki <mmaga@dstribe.net>
2025-02-21 18:19:34 +0900

    refactor: simplify default handling in SettingState logic

--- src/main/kotlin/com/github/naoyukik/intellijplugingithubnotifications/infrastructure/SettingStateRepositoryImpl.kt
+++ src/main/kotlin/com/github/naoyukik/intellijplugingithubnotifications/infrastructure/SettingStateRepositoryImpl.kt
@@ -8,17 +8,12 @@ import com.intellij.openapi.project.Project
 class SettingStateRepositoryImpl(project: Project) : SettingStateRepository {
     private val state = AppSettingsState.getInstance(project)
 
-    companion object {
-        private const val FETCH_INTERVAL_DEFAULT_VALUE = 15
-        private const val RESULT_LIMIT_DEFAULT_VALUE = 30
-    }
-
     override fun loadSettingState(): SettingState {
-        val fetchInterval = state?.myState?.fetchInterval ?: FETCH_INTERVAL_DEFAULT_VALUE
-        val repositoryName = state?.myState?.repositoryName ?: ""
-        val ghCliPath = state?.myState?.ghCliPath?.ifEmpty { "gh" } ?: "gh"
-        val includingRead = state?.myState?.includingRead == true
-        val resultLimit = state?.myState?.resultLimit ?: RESULT_LIMIT_DEFAULT_VALUE
+        val fetchInterval = state.myState.fetchInterval
+        val repositoryName = state.myState.repositoryName
+        val ghCliPath = state.myState.ghCliPath.ifEmpty { "gh" }
+        val includingRead = state.myState.includingRead == true
+        val resultLimit = state.myState.resultLimit
         return SettingState(
             fetchInterval = fetchInterval,
             repositoryName = repositoryName,
