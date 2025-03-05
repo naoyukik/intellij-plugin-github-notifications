8acceab7cb8984b6469f69d678671bf9a58e8129
Kodama Naoyuki <mmaga@dstribe.net>
2025-03-05 22:51:10 +0900

    feat: add ObservableFilterState for filter updates handling

---
+++ src/main/kotlin/com/github/naoyukik/intellijplugingithubnotifications/domain/model/ObservableFilterState.kt
@@ -0,0 +1,25 @@
+package com.github.naoyukik.intellijplugingithubnotifications.domain.model
+
+import kotlin.properties.Delegates
+
+class ObservableFilterState(initialFilter: NotificationFilter) {
+
+    // 登録されたリスナーを格納するリスト
+    private val listeners = mutableListOf<(NotificationFilter) -> Unit>()
+
+    // フィルターの状態を保持
+    var filter: NotificationFilter by Delegates.observable(initialFilter) {
+            _, oldValue, newValue ->
+        if (oldValue != newValue) {
+            listeners.forEach { it(newValue) }
+        }
+    }
+
+    fun addListener(listener: (NotificationFilter) -> Unit) {
+        listeners.add(listener)
+    }
+
+    fun removeListener(listener: (NotificationFilter) -> Unit) {
+        listeners.remove(listener)
+    }
+}
