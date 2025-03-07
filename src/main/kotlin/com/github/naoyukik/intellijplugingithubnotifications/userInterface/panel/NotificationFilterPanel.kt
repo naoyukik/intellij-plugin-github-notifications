c3437b4d153a2c9aa3039ae9c20e25e54bfdb860
Kodama Naoyuki <mmaga@dstribe.net>
2025-03-07 23:20:07 +0900

    refactor: simplify comboBox binding logic in NotificationFilterPanel

--- src/main/kotlin/com/github/naoyukik/intellijplugingithubnotifications/userInterface/panel/NotificationFilterPanel.kt
+++ src/main/kotlin/com/github/naoyukik/intellijplugingithubnotifications/userInterface/panel/NotificationFilterPanel.kt
@@ -1,31 +1,40 @@
 package com.github.naoyukik.intellijplugingithubnotifications.userInterface.panel
 
+import com.github.naoyukik.intellijplugingithubnotifications.domain.model.NotificationFilter
 import com.github.naoyukik.intellijplugingithubnotifications.domain.model.NotificationType
+import com.github.naoyukik.intellijplugingithubnotifications.domain.model.ObservableFilterState
 import com.intellij.openapi.ui.DialogPanel
+import com.intellij.ui.dsl.builder.bindItem
 import com.intellij.ui.dsl.builder.panel
+import javax.swing.JComboBox
 
-class NotificationFilterPanel {
+class NotificationFilterPanel(private val filterState: ObservableFilterState) {
 
     private var selectedType: String? = null
+    private lateinit var typeComboBox: JComboBox<String>
 
-    fun create(): DialogPanel = panel {
-        group("Filters:") {
-            row("Type") {
-                comboBox(
-                    NotificationType.entries.map { it.displayName },
-                ).applyToComponent {
-                        combo ->
-                    addActionListener {
-                        val newValue = combo.selectedItem as? String
-                    }
+    fun create(): DialogPanel {
+        val dialogPanel = panel {
+            group("Filters:") {
+                row("Type") {
+                    typeComboBox = comboBox(
+                        NotificationType.entries.map { it.displayName },
+                    ).bindItem(
+                        getter = { filterState.filter.type },
+                        setter = { newType -> filterState.filter = NotificationFilter(type = newType) },
+                    ).applyToComponent {
+                        addActionListener {
+                            selectedType = this.selectedItem as? String
+                            println("タイプ選択が変更されました: $selectedType")
+                            filterState.filter = NotificationFilter(
+                                type = selectedType,
+                            )
+                        }
+                    }.component
                 }
-                // .bindItem({ selectedType }, { newSelection ->
-                //     selectedType = newSelection
-                //     NotificationFilter(
-                //         type = selectedType,
-                //     )
-                // })
             }
         }
+
+        return dialogPanel
     }
 }
