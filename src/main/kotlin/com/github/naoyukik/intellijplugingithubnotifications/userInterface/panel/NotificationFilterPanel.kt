4b84d665e918f3596ac789de45dc306bfa8fc978
Kodama Naoyuki <mmaga@dstribe.net>
2025-03-11 09:02:08 +0900

    feat: add reviewer-based filtering to notifications

--- src/main/kotlin/com/github/naoyukik/intellijplugingithubnotifications/userInterface/panel/NotificationFilterPanel.kt
+++ src/main/kotlin/com/github/naoyukik/intellijplugingithubnotifications/userInterface/panel/NotificationFilterPanel.kt
@@ -1,9 +1,9 @@
 package com.github.naoyukik.intellijplugingithubnotifications.userInterface.panel
 
-import com.github.naoyukik.intellijplugingithubnotifications.userInterface.filter.NotificationFilter
 import com.github.naoyukik.intellijplugingithubnotifications.userInterface.observable.ObservableFilterState
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.ui.dsl.builder.bindItem
+import com.intellij.ui.dsl.builder.bindText
 import com.intellij.ui.dsl.builder.panel
 import javax.swing.JComboBox
 
@@ -14,21 +14,31 @@ class NotificationFilterPanel(private val filterState: ObservableFilterState) {
 
     fun create(): DialogPanel {
         val dialogPanel = panel {
-            group("Filters:") {
-                row("Type") {
+            group("Filters") {
+                row {
+                    label("Type")
                     typeComboBox = comboBox(
                         NotificationType.entries.map { it.displayName },
                     ).bindItem(
                         getter = { filterState.filter.type },
-                        setter = { newType -> filterState.filter = NotificationFilter(type = newType) },
+                        setter = { newType -> filterState.filter = filterState.filter.copy(type = newType) },
                     ).applyToComponent {
                         addActionListener {
                             selectedType = this.selectedItem as? String
-                            filterState.filter = NotificationFilter(
+                            filterState.filter = filterState.filter.copy(
                                 type = selectedType,
                             )
                         }
                     }.component
+                    textField().bindText(
+                        getter = { filterState.filter.reviewer ?: "" },
+                        setter = { newReviewer ->
+                            filterState.filter = filterState.filter.copy(reviewer = newReviewer)
+                        },
+                    ).applyToComponent {
+                        emptyText.text = "Enter Reviewer"
+                        addActionListener { filterState.filter = filterState.filter.copy(reviewer = this.text) }
+                    }
                 }
             }
         }
