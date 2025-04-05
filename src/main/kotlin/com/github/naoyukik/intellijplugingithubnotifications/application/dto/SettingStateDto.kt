2e7bb548a4801bebaade65d86fac8cffa2fcfd6d
Kodama Naoyuki <mmaga@dstribe.net>
2025-04-05 20:59:16 +0900

    feat: add force refresh option for fetching notifications

--- src/main/kotlin/com/github/naoyukik/intellijplugingithubnotifications/application/dto/SettingStateDto.kt
+++ src/main/kotlin/com/github/naoyukik/intellijplugingithubnotifications/application/dto/SettingStateDto.kt
@@ -3,4 +3,5 @@ package com.github.naoyukik.intellijplugingithubnotifications.application.dto
 data class SettingStateDto(
     val fetchInterval: Int,
     val repositoryName: String,
+    val forceRefresh: Boolean,
 )
