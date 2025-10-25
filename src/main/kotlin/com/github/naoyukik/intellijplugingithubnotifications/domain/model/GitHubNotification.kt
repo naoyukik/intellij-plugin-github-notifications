76792065c99e6302e5f4c1ad1c63179bd98ceed3
Kodama Naoyuki <mmaga@dstribe.net>
2025-10-25 20:53:52 +0900

    refactor: add custom serializer for SubjectType enum

--- src/main/kotlin/com/github/naoyukik/intellijplugingithubnotifications/domain/model/GitHubNotification.kt
+++ src/main/kotlin/com/github/naoyukik/intellijplugingithubnotifications/domain/model/GitHubNotification.kt
@@ -1,8 +1,13 @@
 package com.github.naoyukik.intellijplugingithubnotifications.domain.model
 
 import com.github.naoyukik.intellijplugingithubnotifications.domain.model.NotificationDetailResponse.NotificationDetail
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 
 @Serializable
 data class GitHubNotification(
@@ -30,7 +35,7 @@ data class GitHubNotification(
         val htmlUrl: String,
     )
 
-    @Serializable
+    @Serializable(SubjectTypeSerializer::class)
     enum class SubjectType {
         Issue,
         PullRequest,
@@ -52,6 +57,22 @@ data class GitHubNotification(
                 Release -> "releases"
                 UNKNOWN -> null
             }
+        }
+    }
+
+    object SubjectTypeSerializer : KSerializer<SubjectType> {
+        override val descriptor = PrimitiveSerialDescriptor("SubjectType", PrimitiveKind.STRING)
+
+        override fun serialize(
+            encoder: Encoder,
+            value: SubjectType,
+        ) {
+            encoder.encodeString(value.name)
+        }
+
+        override fun deserialize(decoder: Decoder): SubjectType {
+            val value = decoder.decodeString()
+            return SubjectType.entries.find { it.name == value } ?: SubjectType.UNKNOWN
         }
     }
 }
